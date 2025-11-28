8924d7aba1a6dc2a51ca2f12b0af0b29e2a5413a
Adrien Masurel <adrien.masurel.59@gmail.com>
2025-11-28 12:48:27 +0100

    My first website

--- app.py
+++ app.py
@@ -12,31 +12,24 @@ import requests
 
 "### Which date is the ride?"
 d = st.date_input("",datetime.date.today())
-st.write('Date :', d)
 "\n"
 '### What time?'
 t = st.time_input("",datetime.datetime.now())
-st.write('Time', t)
 "\n"
 "### Select pickup longitude"
 pickup_longitude = st.number_input('', key="pickup_longitude", value=-73.950655)
-st.write('The pickup longitude you chose is : ', pickup_longitude)
 "\n"
 "### Select pick up latitude"
 pickup_latitude = st.number_input('', key="pickup_latitude", value=40.783282)
-st.write('The pickup latitude you chose is : ', pickup_latitude)
 "\n"
 "### Select dropoff longitude"
 dropoff_longitude = st.number_input('', key="dropoff_longitude", value=-73.984365)
-st.write('The dropoff longitude you chose is : ', dropoff_longitude)
 "\n"
 "### Select dropoff latitude"
 dropoff_latitude = st.number_input('', key="dropoff_latitude", value=40.769802)
-st.write('The dropoff latitude you chose is : ', dropoff_latitude)
 "\n"
 "### Select the number of passengers"
 passengers = st.slider("", 0, 10, 10)
-st.write("You selected ", passengers, "passengers")
 
 
 
@@ -60,6 +53,5 @@ prediction = response.get("fare", "I couldn't find anything")
 if st.button('fare'):
     # print is visible in the server output, not in the page
     st.write(prediction)
-    st.write('Further clicks are not visible but are executed')
 else:
-    st.write('I was not clicked 😞')
+    st.write('click me If you want to know the price')
