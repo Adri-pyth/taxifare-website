fee33bb5af4488908e9130cafc093c41d0908ae6
Adrien Masurel <adrien.masurel.59@gmail.com>
2025-11-28 14:25:45 +0100

    My first website

--- app.py
+++ app.py
@@ -12,34 +12,34 @@ import pandas as pd
 
 
 
-"### Which date is the ride?"
+"#### Which date is the ride?"
 d = st.date_input("",datetime.date.today())
-"\n"
-'### What time?'
+
+'#### What time?'
 t = st.time_input("",datetime.datetime.now())
-"\n"
-"### Select pickup longitude"
+
+"#### Select pickup longitude"
 pickup_longitude = st.number_input('', key="pickup_longitude", value=-73.950655)
-"\n"
-"### Select pick up latitude"
+
+"#### Select pick up latitude"
 pickup_latitude = st.number_input('', key="pickup_latitude", value=40.783282)
-"\n"
-"### Select dropoff longitude"
+
+"#### Select dropoff longitude"
 dropoff_longitude = st.number_input('', key="dropoff_longitude", value=-73.984365)
-"\n"
-"### Select dropoff latitude"
+
+"#### Select dropoff latitude"
 dropoff_latitude = st.number_input('', key="dropoff_latitude", value=40.769802)
 "\n"
 
-df = pd.Dataframe({"pickup_longitude": pickup_longitude,
-                   "pickup_latitude": pickup_latitude,
-                   "dropoff_longitude": dropoff_longitude,
-                   "dropoff_latitude": dropoff_latitude})
+df = pd.DataFrame([[pickup_longitude,
+                    pickup_latitude],
+                   [dropoff_longitude,
+                   dropoff_latitude]], columns=["lon", "lat"])
 
 st.map(df)
 
 "### Select the number of passengers"
-passengers = st.slider("", 0, 10, 10)
+passengers = st.slider("", 0, 10, 1)
 
 
 
@@ -62,6 +62,6 @@ prediction = response.get("fare", "I couldn't find anything")
 
 if st.button('fare'):
     # print is visible in the server output, not in the page
-    st.write(prediction)
+    st.write(f"### {round(prediction,2)}$")
 else:
     st.write('click me If you want to know the price')
